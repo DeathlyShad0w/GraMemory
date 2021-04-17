55fb1aadfb4b69f290ebcb8e03e6e1ef0cf315de
wieliczka@giganci.edu.pl <wieliczka@giganci.edu.pl>
2021-04-17 14:59:21 +0200

    test

--- Gra Memoy - DN/Gra memory.cs
+++ Gra Memoy - DN/Gra memory.cs
@@ -20,12 +20,14 @@ namespace Gra_Memoy___DN
             InitializeComponent();
             _settings = new GameSettings();
             UstawKontrolki();
+
+            timerCzasPodgladu.Stop();
         }
 
         private void UstawKontrolki()
         {
-            panelKarty.Width = _settings.Bok * _settings.Kolumny;
-            panelKarty.Height = _settings.Bok * _settings.Wiersze;
+            panelKarty.Width = _settings.Bok * _settings.Wiersze;
+            panelKarty.Height = _settings.Bok * _settings.Kolumny;
 
             Width = panelKarty.Width + 40;
             Height = panelKarty.Height + 100;
@@ -43,11 +45,73 @@ namespace Gra_Memoy___DN
                 Guid id = Guid.NewGuid();
 
                 var b1 = new MemoryCard(id, _settings.PlikLogo, img);
+
+                buttons.Add(b1);
+
+                var b2 = new MemoryCard(id, _settings.PlikLogo, img);
+
+                buttons.Add(b2);
+
             }
+            Random random = new Random();
+
+            panelKarty.Controls.Clear();
+
+            for( int x = 0; x < _settings.Wiersze; x++)
+            {
+                for(int y = 0; y < _settings.Kolumny; y++ )
+                {
+                    var index = random.Next(0, buttons.Count);
+
+                    var b = buttons[index];
+
+                    int margines = 2;
+                    b.Location = new Point((x * _settings.Bok) + (margines * x), (y * _settings.Bok) + (margines * y));
+
+                    b.Width = _settings.Bok;
+                    b.Height = _settings.Bok;
+
+                    b.Odkryj();
+
+                    panelKarty.Controls.Add(b);
+                    buttons.Remove(b); 
+
+
+
+                }
+            }
+
+
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerCzasPodgladu_Tick(object sender, EventArgs e)
+        {
+            _settings.CzasPodgladu--;
+
+            lblStartInfo.Text = $"Początek gry za {_settings.CzasPodgladu}";
+            if (_settings.CzasPodgladu <= 0)
+            {
+                lblStartInfo.Visible = false;
+                
+                foreach(var kontrolka in panelKarty.Controls)
+                {
+                    MemoryCard card = (MemoryCard)kontrolka;
+
+                    card.Zakryj();
+
+                }
+
+                timerCzasPodgladu.Stop();
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
         {
 
         }
